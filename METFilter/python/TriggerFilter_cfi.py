97c5bfbcde16d49257525848c8d3f91143abd381
Michael Wassmer <michael.wassmer@cern.ch>
2022-11-21 11:51:37 +0100

    also include non-isolated lepton triggers

--- METFilter/python/TriggerFilter_cfi.py
+++ METFilter/python/TriggerFilter_cfi.py
@@ -4,9 +4,19 @@ TriggerFilter = cms.EDFilter(
     "FlagFilter",
     filterData = cms.InputTag("TriggerResults","","HLT"),
     filterNames = cms.vstring([
+                      # isolated single muon triggers
                       "HLT_Iso(Mu|TkMu)2[0-9]_v([0-9]|[1-9][0-9])$",
+                      # non-isolated single muon triggers
+                      "HLT_(Mu|TkMu|OldMu)([4-9][0-9]|[1-9][0-9][0-9])_v([0-9]|[1-9][0-9])$",
+                      # pfmet triggers
                       "HLT_PFMET.*",
-                      "HLT_Ele([0-9]|[1-9][0-9])_WPTight_Gsf_v([0-9]|[1-9][0-9])$",
+                      # isolated single electron triggers
+                      "HLT_Ele[2-9][0-9]_WPTight_Gsf_v([0-9]|[1-9][0-9])$",
+                      # non-isolated single electron triggers
+                      "HLT_Ele([4-9][0-9]|[1-9][0-9][0-9])_CaloIdVT_GsfTrkIdT_v([0-9]|[1-9][0-9])$",
+                      # high pt photon triggers
+                      "HLT_Photon[1-9][0-9][0-9]_v([0-9]|[1-9][0-9])$",
+                      # pfht triggers
                       "HLT_PFHT([1-9][0-9][0-9]|[1-9][0-9][0-9][0-9])_v([0-9]|[1-9][0-9])$"
                   ]),
     taggingMode = cms.bool(True),
