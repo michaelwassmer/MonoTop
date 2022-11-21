74de17f81e7c975518da8f49e4e932a63369059b
Michael Wassmer <michael.wassmer@cern.ch>
2022-11-21 11:21:19 +0100

    add IsoTkMu paths and more PFHT paths

--- METFilter/python/TriggerFilter_cfi.py
+++ METFilter/python/TriggerFilter_cfi.py
@@ -4,10 +4,10 @@ TriggerFilter = cms.EDFilter(
     "FlagFilter",
     filterData = cms.InputTag("TriggerResults","","HLT"),
     filterNames = cms.vstring([
-                      "HLT_IsoMu2[0-9]_v([0-9]|[1-9][0-9])$",
+                      "HLT_Iso(Mu|TkMu)2[0-9]_v([0-9]|[1-9][0-9])$",
                       "HLT_PFMET.*",
                       "HLT_Ele([0-9]|[1-9][0-9])_WPTight_Gsf_v([0-9]|[1-9][0-9])$",
-                      "HLT_PFHT(900|1050)_v([0-9]|[1-9][0-9])$"
+                      "HLT_PFHT([1-9][0-9][0-9]|[1-9][0-9][0-9][0-9])_v([0-9]|[1-9][0-9])$"
                   ]),
     taggingMode = cms.bool(True),
     OR_mode = cms.bool(True)
